e1e24c40751b5d15f8c958d5ff284042ed25fcf7
William Preston Lam <lamwillp88@gmail.com>
2025-12-02 08:08:05 -0500

    4.6 vector operations and recycling

---
+++ ch04/4.6.R
@@ -0,0 +1,19 @@
+# vector operations practice
+
+# adding vectors of equal length
+c(5, 10, 15) + c(0, 5, 10)
+
+# concatenate 2 vectors
+texts = c("first", "second", "third")
+texts2 = c("1st", "2nd", "3rd")
+
+paste(texts, texts2)
+
+# vector recycling
+one_word = "string"
+
+# concatenate 2 vectors of unequal length
+paste(texts2, one_word)
+
+
+
