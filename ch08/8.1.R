5d494f91d13b9af5b3ef9f0de53faf7315e6137b
William Preston Lam <lamwillp88@gmail.com>
2025-12-02 20:43:07 -0500

    8.1 referencing cols

---
+++ ch08/8.1.R
@@ -0,0 +1,18 @@
+# load data from ch03
+players = read.csv("ch03/data/Master.csv", stringsAsFactors = FALSE)
+
+# calculate mean weight
+mean(players$weight, na.rm = TRUE)
+
+# calculate sd weight
+sd(players$weight, na.rm = TRUE)
+
+# get columm values row 1:5 in reverse order
+players$weight[5:1]
+
+# count number of unique weight values
+length(unique(players$weight))
+
+# reference multiple columns
+summary(players[, c("height", "weight")])
+
