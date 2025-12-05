792926c6fa4522127a249bc9d3c8e1eb2066f1dc
William Preston Lam <lamwillp88@gmail.com>
2025-12-05 05:48:24 -0700

    14.1 while loops

---
+++ ch14/14.1.R
@@ -0,0 +1,29 @@
+# create vectors
+group = c("A", "B", "C", "D", "E")
+sales = c(19, 60, 82, 72, 26)
+
+# create df
+d = data.frame(group, sales)
+
+# check total sales
+total_sales = sum(d$sales)
+
+# initialize new_sales BEFORE the loop
+d$new_sales = d$sales
+
+# init vals
+growth = 0
+
+while (sum(d$new_sales) < 300) {
+  growth = growth + 0.01
+  d$new_sales = d$sales * (1 + growth)
+  current_total = sum(d$new_sales)
+  print(paste0("s: ", floor(current_total), ", g: ", round(growth, 2)))
+}
+
+# print growth rate ended with
+growth
+
+# print what df eneded with
+d
+
