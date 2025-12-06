0d3c33a8da6233a420fcdb54d6d1dc765f30c4a5
William Preston Lam <lamwillp88@gmail.com>
2025-12-06 07:23:00 -0700

    15.1 custom functions

--- ch15/15.1.R
+++ ch15/15.1.R
@@ -1,3 +1,40 @@
+# data below is in tidy eval, syntax a bit different
+
+# load data from ch03
+players = read.csv("ch03/data/Master.csv", 
+                   stringsAsFactors = FALSE, na.strings="")
+
+# summary by birth month
+players %>%
+  group_by(birthMonth) %>%
+  summarise(players = n()) %>%
+  arrange(desc(players))
+
+# summary by birth country
+players %>%
+  group_by(birthCountry) %>%
+  summarise(players = n()) %>%
+  arrange(desc(players))
+
+# summary by height
+players %>%
+  group_by(height) %>%
+  summarise(players = n()) %>%
+  arrange(desc(players))
+
+# define function with optional rows paramter, def 5 rows
+count_players = function(var, rows = 5) {
+  players %>%
+    group_by({{ var }}) %>%
+    summarise(players = n()) %>%
+    arrange(desc(players)) %>%
+    slice_head(n = rows)
+}
+
+# function calls
+count_players(birthMonth, rows = 10)
+count_players(birthCountry)
+count_players(height)
 
 
 
