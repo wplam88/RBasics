9804af65578e83f2ba822661a48dafad649add0f
William Preston Lam <lamwillp88@gmail.com>
2025-12-08 10:55:55 -0700

    trimming custom functions w/ sapply

--- ch16/16.1.R
+++ ch16/16.1.R
@@ -3,3 +3,41 @@
 list_a = list(c(1:10),
               c(1.01, 1.02, 1.03),
               c(0, 1, 0, 0, 0, 0, 1))
+
+
+# call list_a
+list_a
+
+
+# (l)apply mean function to all vectors in list
+lapply(list_a, mean)
+
+# (s)apply mean function to all vectors in list
+sapply(list_a, mean)
+
+# explore str of lapply() and sapply()
+str(lapply)
+str(sapply)
+
+# apply custom functions
+gap = function(v) {
+  min = min(v)
+  max = max(v)
+  return(max-min)
+}
+
+# apply gap to list_a
+sapply(list_a, gap)
+
+# trim function to replace gap in sapply
+sapply(list_a, function(v) {
+  min = min(v)
+  max = max(v)
+  return(max-min)
+})
+
+# trim function further for brevity
+sapply(list_a, function(v) {max(v) - min(v)})
+
+
+
