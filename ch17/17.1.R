5f167c4232e98fd498da51415de527d8a54a4099
William Preston Lam <lamwillp88@gmail.com>
2025-12-08 14:06:19 -0700

    17.1 sapply

--- ch17/17.1.R
+++ ch17/17.1.R
@@ -9,3 +9,27 @@ space_pos
 first_names = substr(names, 1, space_pos - 1)
 last_names = substr(names, space_pos + 1, nchar(names))
 
+# check structure of strsplit()
+str(strsplit)
+
+name_split = strsplit(names, split = " ")
+name_split
+
+# get the third vector item from name_split
+name_split[[3]]
+
+# get the second item from that vector
+name_split[[3]][2]
+
+# create functions to use in sapply
+first_thing = function(v) {
+  v[1]
+}
+
+last_thing = function(v) {
+  v[2]
+}
+
+# use the functions in sapply
+sapply(name_split, first_thing)
+sapply(name_split, last_thing)
